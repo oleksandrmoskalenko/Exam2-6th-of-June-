7405246bfcd27122687f34a6b7652d2af1b37976
oleksandrmoskalenko <alex_wk@ukr.net>
2020-06-06 16:26:45 +0300

    gallery

--- js/main.js
+++ js/main.js
@@ -4,4 +4,12 @@ $(document).ready(function () {
   // $('.menu__text').mouseover(function () {
   //   $(this).parent().find('.circle').css('background: #fff')
   // })
+
+  $('.slider').slick({
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    pauseOnHover: false,
+    dots: true,
+  })
 })
